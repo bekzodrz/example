44a1cc7b8b1847bdc1419aae11810fbd60a3cc23
bekzod <example@gmail.com>
2024-07-29 11:51:46 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+    // let number = 5.6; // Data type = Number 
+    // console.log(-8 / 0);// Infinity
+    // console.log("Bekzod" * 8); // "NaN" - Not a Number
+
+
+    // Object 
+    // const thief = {
+    //     // key: value 
+    //     age : 30, // number
+    //     jacket : "black", // String
+    //     isLong : false, // boolean
+    // };
+    // console.log(thief.jacket);
+
+
+    // array 
+    // const colors = [ "red", "black", "green", 10, {}, []];
+    // console.log(colors[1]);
+
+
+
+    // const place = confirm("Are you here?");
+    // console.log(place);
+
+    // const currentPlace = prompt("Where are you?", "Samara");
+    // console.log(currentPlace); 
+
+    // const age = prompt("How old are you?", "18");
+    // console.log(age+10);
+
+    // const favouriteColor = [];
+    // favouriteColor[0] = prompt("What's your favourite color #1", ""); 
+    // favouriteColor[1] = prompt("What's your favourite color #2", ""); 
+    // favouriteColor[2] = prompt("What's your favourite color #3", ""); 
+    // console.log(favouriteColor);
+
+    "use strict";
+
+// const bloger = prompt("What's your favorite bloger");
+// console.log("https://instagram.com/" + bloger + "videos");
+
+// Interpolatsia
+
+// const channel = prompt("What's your favorite youtuber");
+// console.log(`https://youtube.com/${channel}`);
+
+// const user = "Bekzod";
+// const job = "Web developer";
+
+// console.log(`${user} is a ${job}`);
+
+// const client = prompt("Hello");
+// console.log("client");
+
+// incriment, decrement 
+
+let incre = 10;
+let decr = 10;
+
+// incre ++;
+// decr --;
+
+// console.log(++incre);
+// console.log(--decr);
+
+// console.log(10 % 3);
+
+
